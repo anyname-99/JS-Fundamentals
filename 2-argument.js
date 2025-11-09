cb3c83732e862c968f21eb5f80eea0a36a9ff6a8
anyname-99 <victorasamoah999@gmail.com>
2025-11-09 16:15:10 +0000

    update 7-multi_c.js

--- 2-argument.js
+++ 2-argument.js
@@ -1,9 +1,10 @@
+
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log("No argument");
+  console.log("0 argument");
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log("1 argument");
 } else {
-  console.log("Arguments found");
+  console.log(`${args.length} arguments`);
 }

e73abf03d95df3692c34978bc1091c47e90ebd8b
anyname-99 <victorasamoah999@gmail.com>
2025-11-09 15:31:09 +0000

    Fix argument message script

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log("0 argument");
+  console.log("No argument");
 } else if (args.length === 1) {
-  console.log("1 argument");
+  console.log("Argument found");
 } else {
-  console.log(`${args.length} arguments`);
+  console.log("Arguments found");
 }

de812b3e7ba99560af7acfcf29ce4e5f85b116c9
anyname-99 <victorasamoah999@gmail.com>
2025-11-09 16:12:20 +0000

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,11 @@
 const arg = process.argv[2];
+
 const count = parseInt(arg);
-if (isNaN(count) || count <= 0) {
+
+if (isNaN(count)) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < Math.abs(count); i++) {
     console.log("C is fun");
   }
 }
